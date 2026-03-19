02245cf21476f6b3e12d93859e1487c2c2aa2eb1
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2026-03-18 21:59:14 -0300

    Submit solution

---
+++ exercises/2026_03_17/q6.m
@@ -0,0 +1,26 @@
+# Exercício 6 - Considerando SPF(10, 5, -3, 3),
+# x = 13.62674, encontre a representação de x em ponto flutuante
+
+beta = 10;
+t = 5;
+emin = -3;
+emax = 3;
+
+x = 13.62674;
+
+% normalização
+e = floor(log10(x));
+m = x / 10^e;
+
+if (e >= emin && e <= emax)
+    printf("X existe em ponto flutuante? Sim\n");
+    % truncamento
+    m_trunc = floor(m * 10^4) / 10^4;
+    x_trunc = m_trunc * 10^e
+
+    % arredondamento
+    m_round = round(m * 10^4) / 10^4;
+    x_round = m_round * 10^e
+else
+    printf("X existe em ponto flutuante? Não\n");
+end
