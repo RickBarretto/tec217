653575d0849e4204f5ff496e61cbecbeea882e01
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2026-03-18 22:06:09 -0300

    Submit solution

---
+++ exercises/2026_03_17/q8.m
@@ -0,0 +1,7 @@
+# Exercício 8 - Indicar os valores usando arredondamento e truncamento 
+
+t = 2;
+
+round((2/3) * 10^(t-1)) / 10^(t-1)
+round(-838 * 10^(t-1)) / 10^(t-1)
+round(99.5 * 10^(t-1)) / 10^(t-1)
