817393cc852fa1b58a1388973052933440c09d4c
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2026-03-18 21:52:08 -0300

    Enhance variables

--- exercises/2026_03_17/q1.m
+++ exercises/2026_03_17/q1.m
@@ -3,17 +3,17 @@
 x = 1/3;
 y = 1/3000;
 
-x_ap = 0.3333;
-y_ap = 0.0003;
+x_aproximado = 0.3333;
+y_aproximado = 0.0003;
 
 % erros absolutos
-ea_x = abs(x - x_ap)
-ea_y = abs(y - y_ap)
+erro_absoluto_x = abs(x - x_aproximado)
+erro_absoluto_y = abs(y - y_aproximado)
 
 % erros relativos
-er_x = ea_x / abs(x)
-er_y = ea_y / abs(y)
+erro_relativo_x = erro_absoluto_x / abs(x)
+erro_relativo_y = erro_absoluto_y / abs(y)
 
 % erros percentuais
-ep_x = er_x * 100
-ep_y = er_y * 100
+erro_percentual_x = erro_relativo_x * 100
+erro_percentual_y = erro_relativo_y * 100
