b2eb583c5b5cbf9ca186834c52a40cbeb24dc054
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2026-03-18 21:53:55 -0300

    Submit solution

---
+++ exercises/2026_03_17/q4.m
@@ -0,0 +1,9 @@
+# Exercício 4 - Considerando SPF(2, 53, -1021, 1024), de base 2
+
+beta = 2; 
+t = 53; 
+emin = -1021; 
+emax = 1024;
+
+menor_numero = beta^(emin)
+maior_numero = (2 - 2^(-52)) * 2^1023
