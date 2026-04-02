4f9893f4d7a8b30b0752e3f5e7efbdb8c012853f
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2026-04-02 15:44:29 -0300

    Submit solution

---
+++ exercises/2026_03_31/linear_iter.m
@@ -0,0 +1,35 @@
+% Método da Iteração Linear
+
+f = @(x) x^2 - 3*x + exp(x) - 2;
+g = @(x) (x^2 + exp(x) - 2) / 3;
+
+x0 = -0.5;
+d = 1e-4;
+N = 2;
+
+xr = x0;
+ea = Inf;
+
+for iter = 1:N
+  xrant = xr;
+  xr = g(xrant);
+
+  if xr != 0
+    ea = abs((xr - xrant) / xr) * 100;
+  end
+
+  if ea < d || iter >= N
+    printf('Raiz: %.6f | Iteracoes: %d | Erro: %.6f%%\n', xr, iter, ea);
+    return
+  end
+end
+
+printf('Metodo falhou em %d iteracoes.\n', N);
+
+% Saída esperada
+%
+% No caso de x0 = -1:
+% Raiz: -0.381864 | Iteracoes: 2 | Erro: 44.821452%
+%
+% No caso de x0 = -0.5:
+% Raiz: -0.390550 | Iteracoes: 2 | Erro: 2.405107%
